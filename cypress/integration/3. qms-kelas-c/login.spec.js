75bada85a11170d73944769ba72c8aa295663b24
Ari Cahyono <cahyono.ari80@gmail.com>
2021-09-30 12:13:20 +0700

    feat : add login test

--- cypress/integration/3. qms-kelas-c/login.spec.js
+++ cypress/integration/3. qms-kelas-c/login.spec.js
@@ -1,13 +1,63 @@
-describe("Login Page authentication-integration app", () => {
-  it("successfully loads", () => {
-    cy.visit("http://127.0.0.1:3000");
+  describe("Test Access Login Page", () => {
+    beforeEach(() => {
+      cy.wait(2000);
+      cy.visit("/");
+    });
+  it("Test access Beranda Page with not Login", () => {
+    cy.visit("/beranda");
+    cy.url().should("include", "/");
+    });
+    it("Test access Booking Queue Page with not Login", () => {
+      cy.visit("/book-nomor-antrian");
+      cy.url().should("include", "/");
+    });
+  it("Test access List Bank Page with not Login", () => {
+    cy.visit("/daftar-bank");
+    cy.url().should("include", "/");
   });
-  it("wait for document network call", () => {
-    cy.visit("http://localhost:3000");
-    cy.get("input[id='username']").type("dts2021@tes.com");
-    cy.wait(1000);
-    cy.get("input[id='password']").type("admin");
-    cy.wait(1000);
-    cy.get("form").submit();
+  it("Test access Bank Queue Detail Page with not Login", () => {
+    cy.visit("/info-antrian");
+    cy.url().should("include", "/");
   });
-});
+
+  it("Test load login page", () => {
+    cy.visit("/");
+    cy.url().should("include", "/");
+  });
+
+  });
+
+  describe("Test Form Login in Login Page", () => {
+    beforeEach(() => {
+      cy.wait(2000);
+      cy.visit("/");
+    });
+
+    it("Test load content login", () => {
+      cy.get("form").submit();
+      cy.get("span").should("contain", "Belum punya akun? Daftar");
+    });
+
+    it("Test form login with empty data", () => {
+      cy.get("form").submit();
+      cy.get("span").should("contain", "login gagal");
+    });
+
+    it("Test form login with wrong data", () => {
+      cy.get("input[id='username']").type("dts2021@tesdwadwa.com");
+      cy.wait(1000);
+      cy.get("input[id='password']").type("dts2021");
+      cy.wait(1000);
+      cy.get("form").submit();
+      cy.get("span").should("contain", "login gagal");
+    });
+
+    it("wait for document network call", () => {
+      cy.get("input[id='username']").type("dts2021@tes.com");
+      cy.get("input[id='password']").type("admin");
+      cy.wait(1000);
+      cy.get("form").submit();
+      cy.url().should("include", "/beranda");
+    });
+  });
+
